d927946ab99aadc6820eb68312bcbc4125ebf52d
arlindabeqiraj <arlinda.beqiraj1@student.uni-pr.edu>
2024-01-11 05:09:12 +0100

    commit

--- d.js
+++ d.js
@@ -1,34 +1,31 @@
-const dragPhoto = document.getElementById('dragPhoto');
+const draggableElement = document.getElementById('dragPhoto');
 let isDragging = false;
 
-dragPhoto.addEventListener('mousedown', function(event) {
+draggableElement.addEventListener('mousedown', function(startEvent) {
   isDragging = true;
-  dragPhoto.style.cursor = 'grabbing';
-  const offsetX = event.clientX - dragPhoto.getBoundingClientRect().left;
-  const offsetY = event.clientY - dragPhoto.getBoundingClientRect().top;
-
-  // Set transition property before dragging starts
-  dragPhoto.style.transition = 'transform 0.3s ease-in-out';
+  draggableElement.style.cursor = 'grabbing';
+  const offsetX = startEvent.clientX - draggableElement.getBoundingClientRect().left;
+  const offsetY = startEvent.clientY - draggableElement.getBoundingClientRect().top;
+  draggableElement.style.transition = 'transform 0.3s ease-in-out';
 
   document.addEventListener('mousemove', dragElement);
   document.addEventListener('mouseup', function() {
     isDragging = false;
-    dragPhoto.style.cursor = 'grab';
+    draggableElement.style.cursor = 'grab';
 
     document.removeEventListener('mousemove', dragElement);
-
-    // Delay setting the transform property until after the transition is applied
+    
     setTimeout(() => {
-      dragPhoto.style.transition = '';
-      dragPhoto.style.transform = 'translate(0, 0)';
+      draggableElement.style.transition = '';
+      draggableElement.style.transform = 'translate(0, 0)';
     }, 0);
   });
 
-  function dragElement(event) {
+  function dragElement(moveEvent) {
     if (isDragging) {
-      const x = event.clientX - offsetX;
-      const y = event.clientY - offsetY;
-      dragPhoto.style.transform = `translate(${x}px, ${y}px)`;
+      const x = moveEvent.clientX - offsetX;
+      const y = moveEvent.clientY - offsetY;
+      draggableElement.style.transform = `translate(${x}px, ${y}px)`;
     }
   }
 });
